f6ffd22da3a18784735a1bc021bed4015cbdb662
Artem Ismagilov <artem.ismagilov.2000@mail.ru>
2023-07-07 06:09:11 +0300

    change bind gunicorn from exteranl host to interanl

--- app_integration/gunicorn.conf.py
+++ app_integration/gunicorn.conf.py
@@ -3,7 +3,7 @@ import multiprocessing
 
 from settings import APP_HOST_INTERNAl, APP_HOST_EXTERNAL, APP_PORT
 #workers = multiprocessing.cpu_count() * 2 + 1
-bind = f"{APP_HOST_EXTERNAL}:{APP_PORT}"
+bind = f"{APP_HOST_INTERNAl}:{APP_PORT}"
 accesslog = '-'
 access_log_format = 'h:%(h)s l:%(l)s u:%(u)s t:%(t)s r:"%(r)s" s:%(s)s b:%(b)s f:"%(f)s" a:"%(a)s"\n________________'
 wsgi_app = 'start:create_app()'
