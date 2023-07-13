9c66b35f177b7367093a081b9f209eeab44b030b
Artem Ismagilov <artem.ismagilov.2000@mail.ru>
2023-07-13 19:08:57 +0300

    replace glob.glob on subprocess module

--- app_integration/patch_driver.py
+++ app_integration/patch_driver.py
@@ -1,13 +1,14 @@
-import glob
-path_to_mattermost = glob.glob('/usr/local/lib/python3.11/site-packages/mattermostdriver/websocket.py', recursive=True)
-p = path_to_mattermost.pop()
-print(p, flush=True)
-
-with open(p, mode='r+', encoding='utf-8') as websocket_file:
-    old_text = websocket_file.read()
-    new_text = old_text.replace('CLIENT_AUTH', 'SERVER_AUTH', 1)
-    websocket_file.truncate(0)
-    websocket_file.seek(0)
-    websocket_file.write(new_text)
+import subprocess
 
 
+def patch_mattermostdriver():
+    result = subprocess.run('find / -type f -name "websocket.py"', shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    print(f"{result.stdout=}", flush=True)
+    path_file = result.stdout.strip()
+    if path_file:
+        data = open(path_file, mode='r', encoding='utf-8').read().replace('CLIENT_AUTH', 'SERVER_AUTH', 1)
+        open(path_file, mode='w', encoding='utf-8').write(data)
+
+
+if __name__ == '__main__':
+    patch_mattermostdriver()
